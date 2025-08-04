f1f09f4e694581beb98cd51bb9eb14136807aeaa
91866 <dhivyaasuresh01@gmail.com>
2025-08-04 22:56:45 +0530

    datatypes commit

---
+++ src/day1_basic/Datatypes.java
@@ -0,0 +1,19 @@
+package day1_basic;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int age = 20;
+	        float marks = 85.5f;
+	        char grade = 'A';
+	        boolean passed = true;
+
+	        System.out.println("Age: " + age);
+	        System.out.println("Marks: " + marks);
+	        System.out.println("Grade: " + grade);
+	        System.out.println("Passed: " + passed);
+
+	}
+
+}
