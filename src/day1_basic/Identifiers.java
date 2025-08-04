a70a8e57342cdebcbc99d565965845eb1cf41484
91866 <dhivyaasuresh01@gmail.com>
2025-08-04 23:02:12 +0530

    identifiers commit

---
+++ src/day1_basic/Identifiers.java
@@ -0,0 +1,15 @@
+package day1_basic;
+
+public class Identifiers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 21;
+        String name = "Dhivyaa";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+
+	}
+
+}
