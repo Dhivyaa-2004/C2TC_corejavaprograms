55aa7060aa9bf808f753647e73defb3374ace721
91866 <dhivyaasuresh01@gmail.com>
2025-08-04 22:49:18 +0530

    initial commit

---
+++ src/day1_basic/SampleDemo.java
@@ -0,0 +1,11 @@
+package day1_basic;
+
+public class SampleDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is the sample program");
+
+	}
+
+}
