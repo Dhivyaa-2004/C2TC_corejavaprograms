d92ee4f2a7677386b8585ec51e9270f3fbbc1c85
91866 <dhivyaasuresh01@gmail.com>
2025-08-06 00:07:09 +0530

    updated

---
+++ src/com/tnsif/Day1/Identifiers.java
@@ -0,0 +1,11 @@
+package com.tnsif.Day1;
+
+public class Identifiers {
+    public static void main(String[] args) {
+        int $age = 25;
+        String name = "Arun";
+
+        System.out.println("Age: " + $age);
+        System.out.println("Name: " + name);
+    }
+}
