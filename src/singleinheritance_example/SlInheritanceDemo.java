bc23fbfbf59996e45f8895e8a3ad98491cf18764
DhivyaaSuresh <DhivyaaSuresh@DESKTOP-R9EOCKT>
2025-08-19 14:10:59 +0530

    Single inheritance updated

---
+++ src/singleinheritance_example/SlInheritanceDemo.java
@@ -0,0 +1,14 @@
+package singleinheritance_example;
+
+public class SlInheritanceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		Trainer t=new Trainer(122,"ARUN","ALAPUZHA",301,"DEVELOPER","CHENNAI");
+		System.out.println(t);
+		
+		Person p=new Person (311,"NITHYA","PALAYAM");
+		System.out.println(p);
+	}
+
+}
