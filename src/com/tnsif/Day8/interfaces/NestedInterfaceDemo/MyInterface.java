d6ea85f1c0015be909d98d317a30f23a802ea24d
Your Name <your-email@example.com>
2025-08-21 22:43:04 +0530

    Day 8 Updated

---
+++ src/com/tnsif/Day8/interfaces/NestedInterfaceDemo/MyInterface.java
@@ -0,0 +1,10 @@
+package com.tnsif.Day8.interfaces.NestedInterfaceDemo;
+
+public abstract class MyInterface {
+	 abstract void calculateArea();
+	    interface MyInnerInterface {
+	       int  id = 20;
+	       void print();     
+	    }
+
+}
