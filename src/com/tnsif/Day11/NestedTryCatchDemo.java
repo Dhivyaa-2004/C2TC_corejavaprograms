79b77d7c9255f3fde1a4772fcf6ec3334b89729c
Your Name <your-email@example.com>
2025-08-26 10:32:45 +0530

    Day 11 Updated

---
+++ src/com/tnsif/Day11/NestedTryCatchDemo.java
@@ -0,0 +1,29 @@
+package com.tnsif.Day11;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class NestedTryCatchDemo {
+    public static void main(String[] a) {
+        String name;
+        int pos;
+
+        try (Scanner sc = new Scanner(System.in)) {
+            try {
+                System.out.println("Enter your name: ");
+                name = sc.next();
+                System.out.println("Enter the position: ");
+                pos = sc.nextInt();
+
+                try {
+                    System.out.println("Character at index " + pos + " in '" + name + "' is " + name.charAt(pos));
+                } catch (StringIndexOutOfBoundsException e) {
+                    System.out.println("Error: " + e.getMessage());
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter an integer.");
+            }
+        }
+    }
+}
