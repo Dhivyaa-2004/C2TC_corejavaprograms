d19c69655a65b6f28099ff8c65ecf1057d3e783f
Your Name <your-email@example.com>
2025-08-26 11:58:06 +0530

    Day 12 Updated

---
+++ src/com/tnsif/Day12/userdefinedexp/InvalidMarksException.java
@@ -0,0 +1,17 @@
+package com.tnsif.Day12.userdefinedexp;
+
+public class InvalidMarksException extends Exception {
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super();
+	}
+
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
